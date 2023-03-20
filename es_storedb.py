93b4375c815c1c6cf2ae7d376de5afa3ffeade95
kittypark <kittypark.github.io@gmail.com>
2023-03-20 15:46:44 +0900

    docs : Change Dir

--- es_storedb.py
+++
@@ -1,9 +0,0 @@
-from elasticsearch import Elasticsearch
-import es_data
-es = Elasticsearch(hosts=["https://localhost:9200"],verify_certs=False,http_auth=("elastic", "241900"))
-
-data = es_data.load_data()
-for i in range(1,len(data)):
-    es.index(index="stock", id=i, body=data[i-1])
-
-
