9a703fdfde211164dde6a95f72568661d66be999
kittypark <kittypark.github.io@gmail.com>
2023-03-20 16:22:52 +0900

    feat : CRUD Create

--- CRUD/Create/Program.cs
+++ CRUD/Create/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using Nest;
 
 namespace Create
 {
+    public class MyDocument
+    {
+        public string company { get; set; }
+        public string code { get; set; }
+    }
+
     class Program
     {
-        static void Create(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var uri = new Uri("http://localhost:9200");
+            var connectionSettings = new ConnectionSettings(uri)
+                .BasicAuthentication("elastic", "241900")
+                .DefaultIndex("stock");
+
+            var client = new ElasticClient(connectionSettings);
+
+            // 인덱스 생성 또는 업데이트
+            var createIndexResponse = client.Indices.Create("test", c => c
+                .Settings(s => s
+                    .NumberOfShards(1)
+                    .NumberOfReplicas(0)
+                )
+                .Map<MyDocument>(m => m
+                    .AutoMap()
+                )
+            );
+
+            // 문서 추가
+            var document = new MyDocument
+            {
+                company = "Kitty2",
+                code = "88888"
+            };
+            client.IndexDocument(document);
         }
     }
 }
