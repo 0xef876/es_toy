dcb87fad7f6376a9058fa874b532c7707ee7c553
kittypark <kittypark.github.io@gmail.com>
2023-03-20 16:30:44 +0900

    feat : CRUD Read

--- CRUD/Read/Program.cs
+++ CRUD/Read/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using Nest;
 
 namespace Read
 {
+    public class MyDocument
+    {
+        public string company { get; set; }
+        public string code { get; set; }
+    }
+
     class Program
     {
-        static void Read(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var uri = new Uri("http://localhost:9200");
+            var connectionSettings = new ConnectionSettings(uri)
+                .DefaultIndex("stock")
+                .BasicAuthentication("elastic", "241900");
+
+            var client = new ElasticClient(connectionSettings);
+
+            // 검색 쿼리 작성
+            var searchResponse = client.Search<MyDocument>(s => s
+                .Query(q => q
+                    .Match(m => m
+                        .Field(f => f.company)
+                        .Query("Kitty")
+                    )
+                )
+            );
+
+            // 검색 결과 출력
+            foreach (var hit in searchResponse.Hits)
+            {
+                Console.WriteLine($"Company: {hit.Source.company}, Code: {hit.Source.code}");
+            }
         }
     }
 }
